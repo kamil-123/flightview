1c5f2ec625452aa55a4eb982c8c096a524136552
Kamil Drozd <drozdk@centrum.cz>
2020-03-13 14:35:27 +0100

    transrofmation of destinations added and props for Searchresults

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,33 @@ function App() {
   const [selectedItemFrom, setSelectedItemFrom] = useState('Select From destination')
   const [selectedItemTo,setSelectedItemTo] = useState('Select To destination')
   const [searched, setSearched ] = useState(false)
+  const [destTo,setDestTo] = useState('')
+  const [destFrom,setDestFrom] = useState('')
   
   const handleKeyPress = (e) => {
     if(e.key === 'Enter') {
       handleSearchClick()
   }
     } 
+
+  const destinatios = {
+    'Prague':'PRG',
+    'Berlin':'BER',
+    'Warsaw':'WAW',
+    'Pardubice':'PED',
+    'Valencia':'VLC',
+    'Barcelona':'BCN',
+    'Madrid':'MAD',
+    'Milano':'MXP',
+    'Athens':'AHN',
+  }
+
   const handleSearchClick = () => {
+    setDestFrom(destinatios[selectedItemFrom])
+    setDestTo(destinatios[selectedItemTo])
+    // console.log(destFrom)
+    // console.log(destTo)
+    
     setSearched(true)
     
   }
@@ -36,7 +56,7 @@ function App() {
         setSelectedItemTo={setSelectedItemTo}
       />
       <br />
-      < SearchResults searched={searched} />
+      < SearchResults searched={searched} destFrom={destFrom} destTo={destTo} />
     </div>
     )
   
