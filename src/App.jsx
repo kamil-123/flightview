b9e22a5764d18ae43e9275c521af8b25c8c3b082
Martin <martin.setina@seznam.cz>
2020-03-13 09:48:32 +0100

    added search components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import SearchBar from './Components/SearchBar.jsx'
+import SearchResults from './Components/SearchResults'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Start Here
+        < SearchBar />
+        < SearchResults />
     </div>
     );
   }
