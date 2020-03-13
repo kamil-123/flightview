b443c7c4d5ff63aeb46ba1ae274f4767bdc7fb0d
Martin <martin.setina@seznam.cz>
2020-03-13 11:58:17 +0100

    search results added, fetching from API, still button isnt hidden when last results are shown

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -1,27 +1,70 @@
 import React, { useState, useEffect } from 'react';
 import { DateTime } from 'luxon';
 
-const searchFlights = (origin='PRG', destination='CDG', direct='1') => {
-  const when = DateTime.local().plus({ days: 1 }).toFormat('dd/MM/yyyy');
-  const query = new URLSearchParams({
-    partner: 'picky',
-    fly_from: origin,
-    fly_to: destination,
-    date_from: when,
-    direct_flights: direct,
-    limit: 5
-  })
-
-  const url = new URL(`?${query}`, 'https://api.skypicker.com/flights')
-  console.log('url', url)
-
-}
 
 function SearchResults(props) {
+  const [ flightResults, setFlightResults] = useState([])
+  const [ numberOfResults, setNumberOfResults ] = useState(5)
+  const [ hideButton, setHideButton ] = useState(false)
+  const [ dataNumberOfResults, setDataNumberOfResults] = useState(0)
+  const [ loading, setLoading ] = useState(true)
+
+
+  useEffect(() => {
+    getFlights()}, [numberOfResults])
+
+  useEffect(() => {
+
+
+  })
+
+  const handleMoreResultsClick = () => {
+      setNumberOfResults(numberOfResults + 5)
+  }
+
+  const getFlights = async (origin='PRG', destination='CDG', direct='1') => {
+    const when = DateTime.local().plus({ days: 1 }).toFormat('dd/MM/yyyy');
+    const query = new URLSearchParams({
+      partner: 'picky',
+      fly_from: origin,
+      fly_to: destination,
+      date_from: when,
+      direct_flights: direct,
+      limit: numberOfResults
+    })
+    const url = new URL(`?${query}`, 'https://api.skypicker.com/flights')
+    try {
+      const promise = await fetch(url)
+      const data = await promise.json()
+      setFlightResults(data.data)
+      setDataNumberOfResults(data._results)
+      setLoading(false)
+
+    } catch(err) {
+      console.log('error fetching flights', err)
+    }
+  }
+
+  const renderedFlights = flightResults.map(data => {
+    let departureDate = DateTime.fromMillis(data.dTime * 1000).toFormat('hh:mm')
+    let arrivalDate = DateTime.fromMillis(data.aTime * 1000).toFormat('hh:mm')
+    return (
+      <div key={data.id}>
+        <p>From: {data.cityFrom} at {departureDate}</p>
+        <p>To: {data.cityTo} at {arrivalDate}</p>
+        <p>Length: {data.fly_duration}</p>
+        <p> Price: {data.price}</p>
+        <hr />
+      </div>
+    )
+  })
+    
 
   return (
     <div>
-      Hello
+      {(loading) ? "Page is loading" : renderedFlights}
+      <br />
+      <button hidden={hideButton} onClick={handleMoreResultsClick}> Show more flights </button>
     </div>
   )
 }
